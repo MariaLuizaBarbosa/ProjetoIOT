12123ac97ef5775fa503b8e5bb764dde7ef35d70
MariaLuizaBarbosa <malubarbosa2708@gmail.com>
2025-09-30 15:45:41 -0300

    Falta arrumar o ambiente create

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,18 @@
 <?php
 
-<<<<<<< HEAD
 use App\Livewire\Ambiente\AmbienteCreate;
 use App\Livewire\Ambiente\AmbienteEdit;
 use App\Livewire\Ambiente\AmbienteList;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/ambiente/create', AmbienteCreate::class)->name('ambiente.create');
-Route::get('/ambiente/list', AmbienteList::class)->name('ambiente.list');
-Route::get('/ambiente/edit/{id}', AmbienteEdit::class)->name('ambiente.edit');
-=======
 use App\Livewire\Dashboard;
 use App\Livewire\Sensor\SensorCreate;
 use App\Livewire\Sensor\SensorEdit;
 use App\Livewire\Sensor\SensorList;
-use Illuminate\Support\Facades\Route;
+
+Route::get('/ambiente/create', AmbienteCreate::class)->name('ambiente.create');
+Route::get('/ambiente/list', AmbienteList::class)->name('ambiente.list');
+Route::get('/ambiente/edit/{id}', AmbienteEdit::class)->name('ambiente.edit');
 
 Route::get('/sensor/create', SensorCreate::class)->name('sensor.create');
 Route::get('/sensor/list', SensorList::class)->name('sensor.list');
 Route::get('/sensor/edit/{id}', SensorEdit::class)->name('sensor.edit');
->>>>>>> 22d963431113eec93efcf9915758080887d4fafe
