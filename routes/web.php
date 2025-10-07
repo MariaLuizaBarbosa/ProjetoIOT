0e8c66d20cbf35bd9b49b86ec70fc1a44b677702
MariaLuizaBarbosa <malubarbosa2708@gmail.com>
2025-10-07 08:15:39 -0300

    arrumando

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/ambiente/edit/{id}', AmbienteEdit::class)->name('ambiente.edit');
 Route::get('/sensor/create', SensorCreate::class)->name('sensor.create');
 Route::get('/sensor/list', SensorList::class)->name('sensor.list');
 Route::get('/sensor/edit/{id}', SensorEdit::class)->name('sensor.edit');
+
+Route::get('/', Dashboard::class);
